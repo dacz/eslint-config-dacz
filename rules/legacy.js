de840fc80dd2f9fbf9ea6fe77f7face58c0878a7
David C <david@origamica.com>
2017-04-22 13:23:25 +0200

    experimenting with warnings about complexity2

--- rules/legacy.js
+++ rules/legacy.js
@@ -1,11 +1,12 @@
 module.exports = {
   'rules': {
+    'complexity': [1, 4],
     // specify the maximum depth that blocks can be nested
-    'max-depth': [0, 4],
+    'max-depth': [1, 4],
     // limits the number of parameters that can be used in the function declaration.
-    'max-params': [0, 3],
+    'max-params': [1, 4],
     // specify the maximum number of statement allowed in a function
-    'max-statements': [0, 10],
+    'max-statements': [1, 10],
     // disallow use of bitwise operators
     'no-bitwise': 0,
     // disallow use of unary operators, ++ and --
