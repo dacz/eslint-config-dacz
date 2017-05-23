1770419887a233b214f95a1810766d159f4fb4f8
David C <david@origamica.com>
2017-05-23 09:03:44 +0200

    comma dangle for consistent prettier

--- rules/es6.js
+++ rules/es6.js
@@ -1,15 +1,15 @@
 module.exports = {
-  'rules': {
+  rules: {
     // enforces no braces where they can be omitted
     // http://eslint.org/docs/rules/arrow-body-style
-    'arrow-body-style': [2, 'as-needed'],
+    'arrow-body-style': [ 2, 'as-needed' ],
     // require parens in arrow function arguments
     'arrow-parens': 0,
     // require space before/after arrow function's arrow
     // http://eslint.org/docs/rules/arrow-spacing
-    'arrow-spacing': [2, { 'before': true, 'after': true }],
+    'arrow-spacing': [ 2, { before: true, after: true } ],
     // require trailing commas in multiline object literals
-    'comma-dangle': 0,
+    'comma-dangle': [ 'error', 'always-multiline' ],
     // verify super() callings in constructors
     'constructor-super': 0,
     // enforce the spacing around the * in generator functions
@@ -51,12 +51,15 @@ module.exports = {
     'no-useless-constructor': 2,
     // require method and property shorthand syntax for object literals
     // http://eslint.org/docs/rules/object-shorthand
-    'object-shorthand': [2, 'always'],
+    'object-shorthand': [ 2, 'always' ],
     // suggest using arrow functions as callbacks
-    'prefer-arrow-callback': [2, {
-      'allowNamedFunctions': false,
-      'allowUnboundThis': true,
-    }],
+    'prefer-arrow-callback': [
+      2,
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true
+      }
+    ],
     // suggest using of const declaration for variables that are never modified after declared
     'prefer-const': 2,
     // suggest using the spread operator instead of .apply()
@@ -79,6 +82,6 @@ module.exports = {
     'template-curly-spacing': 2,
     // enforce spacing around the * in yield* expressions
     // http://eslint.org/docs/rules/yield-star-spacing
-    'yield-star-spacing': [2, 'after'],
+    'yield-star-spacing': [ 2, 'after' ]
   }
 };
