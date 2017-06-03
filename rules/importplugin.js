41e1e6b7c27fb88ef7526f4e5c7c9a4f307cd082
David C <david@origamica.com>
2017-06-03 20:58:49 +0200

    devDeps import

--- rules/importplugin.js
+++ rules/importplugin.js
@@ -1,23 +1,31 @@
 module.exports = {
   rules: {
-    'import/no-unresolved': [ 2, { commonjs: true } ],
+    'import/no-unresolved': [2, { commonjs: true }],
     'import/default': 2,
     'import/no-deprecated': 1,
     'import/export': 2,
     'import/no-extraneous-dependencies': [
       2,
       {
-        devDependencies: [ '**/*.test.js', '**/*.spec.js', 'tests' ],
+        devDependencies: ['**/*.test.js', '**/*.spec.js', 'tests', 'tests/**/*.js'],
         optionalDependencies: false,
       },
     ],
     'import/no-amd': 2,
     'import/no-mutable-exports': 2,
+    // 'sort-imports': [
+    //   'error',
+    //   {
+    //     ignoreCase: false,
+    //     ignoreMemberSort: false,
+    //     memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+    //   },
+    // ],
   },
   settings: {
     'import/resolver': {
       node: {
-        extensions: [ '.js', '.json' ],
+        extensions: ['.js', '.json'],
       },
     },
   },
