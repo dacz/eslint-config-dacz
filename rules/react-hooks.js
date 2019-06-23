2d3c074225739a7a9f15bf42834cbd53e0cbe14b
DavidC <david@dchere.com>
2019-06-23 15:42:33 +0200

    react-hooks

---
+++ rules/react-hooks.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
+};
